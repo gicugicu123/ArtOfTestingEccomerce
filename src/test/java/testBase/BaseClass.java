7c24cf25260f6c18185c44c02802969f5a5ed8b6
Adi <gicubere123@gmail.com>
2024-10-09 10:26:34 +0300

    added xml and cross browser handling

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass
 {
@@ -13,11 +16,20 @@ public class BaseClass
 	public Logger logger; //Log4j
 	
 	@BeforeClass
-	public void setup()
+	@Parameters({"browser", "os"})
+	public void setup(String browser, String os)
 	{
 		logger = (Logger) LogManager.getLogger(this.getClass());
 		
-		driver = new ChromeDriver();
+		switch (browser)
+		{
+			case "chrome": driver = new ChromeDriver(); break;
+			case "edge": driver = new EdgeDriver(); break;
+			case "firefox": driver = new FirefoxDriver(); break;
+			
+			default: logger.error("Invalid browser");
+		}
+		
 		
 		driver.get("https://ecommerce.artoftesting.com/");
 		
--- src/test/java/testCases/HomePageTest.java
+++ src/test/java/testCases/HomePageTest.java
@@ -9,10 +9,12 @@ import testBase.BaseClass;
 public class HomePageTest extends BaseClass
 {	
 	@Test
-	public void isSelectDisplayed()
+	public void isSelectDisplayed() throws InterruptedException
 	{
 		HomePage homePage = new HomePage(driver);
 		boolean isDisplayed = homePage.isDropdownDisplayed();
+		
+		Thread.sleep(2000);
 		
 		logger.info("Check that dropdown is displayed");
 		Assert.assertEquals(isDisplayed, true);
